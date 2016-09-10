3568331d962c7f38cb26119b2a149069e66ff4ef
ZackExplosion <cstony0917@gmail.com>
2016-09-11 00:33:06 +0800

    setup go lang iris web framework

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
-FROM golang:1.6-onbuild
+FROM golang:1.7.1
+RUN go get -u github.com/kataras/iris/iris
+RUN go get -u github.com/iris-contrib/template/html
+EXPOSE 8080
--- app/main.go
+++ app/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import "github.com/kataras/iris"
 
 func main() {
-	fmt.Println("Hello, 世界")
+		// iris.Favicon("./favicon.ico")
+		iris.Config.IsDevelopment = true
+		iris.Get("/", func(ctx *iris.Context) {
+		    ctx.Render("index.html",  struct{ Name string }{Name: "iris"})
+				// ctx.Render("text/plain", "hello world")
+		})
+
+		// iris.Get("/login", func(ctx *iris.Context) {
+		//     ctx.Render("login.html", iris.Map{"Title": "Login Page"})
+		// })
+
+		// iris.Post("/login", func(ctx *iris.Context) {
+		//     secret := ctx.PostValue("secret")
+		//     ctx.Session().Set("secret", secret)
+
+		//     ctx.Redirect("/user")
+		// })
+
+		iris.Listen(":8080")
 }
